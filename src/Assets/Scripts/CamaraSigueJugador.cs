f94b71f75329d7ccd874b921886a86f7004a68f6
Joseph Zamora <joseph.zamora64@yahoo.com>
2018-08-15 12:46:11 -0600

    Bakear el lighting, Crear transición de camaras

--- src/Assets/Scripts/CamaraSigueJugador.cs
+++ src/Assets/Scripts/CamaraSigueJugador.cs
@@ -1,5 +1,10 @@
 ﻿using UnityEngine;
 
+enum ModosDeCamara
+{
+	_3D,_2D
+}
+
 [DisallowMultipleComponent]
 public class CamaraSigueJugador : MonoBehaviour {
 
@@ -9,7 +14,11 @@ public class CamaraSigueJugador : MonoBehaviour {
 	[Range(0f,1f)]
 	private float velocidad;
 	[SerializeField]
-	private Vector3 offset;
+	private Vector3 offset3D;
+	[SerializeField]
+	private Vector3 offset2D;
+	[SerializeField]
+	private ModosDeCamara modoCamara;
 	#endregion
 
 	#region Metodos de Unity
@@ -26,10 +35,27 @@ public class CamaraSigueJugador : MonoBehaviour {
 
 	private void AjustarPosicionRotacion()
 	{
-		Vector3 posDeseada = nave.position + offset;
+		if (modoCamara == ModosDeCamara._3D)
+			modo3D();
+		else
+			modo2D();
+	}
+
+	private void modo3D()
+	{
+		Vector3 posDeseada = nave.position + offset3D;
 		Vector3 posRefinada = Vector3.Lerp(transform.position, posDeseada, velocidad);
 		transform.position = posRefinada;
 
 		transform.LookAt(nave);
+	}
+
+	private void modo2D()
+	{
+		Vector3 posDeseada = nave.position + offset2D;
+		Vector3 posRefinada = Vector3.Lerp(transform.position, posDeseada, velocidad);
+		posRefinada.y = 3357f;
+		posRefinada.x = 0;
+		transform.position = posRefinada;
 	}
 }
---
+++ src/Assets/Scripts/cambiarCamara.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class cambiarCamara : MonoBehaviour {
+
+	#region Variables
+	private GameObject[] camaras;//1er camara es 3D, 2da es 2D
+	[SerializeField]
+	private KeyCode tecla;
+	#endregion
+
+	#region Metodos de Unity
+	private void Start()
+	{
+		camaras = GameObject.FindGameObjectsWithTag("camaras");
+	}
+
+	void Update () {
+		bool presionaTecla = Input.GetKeyDown(tecla);
+		if (presionaTecla)
+		{
+			toggleCamara();
+		}
+    }
+	#endregion
+
+	void toggleCamara()
+	{
+		foreach (var cam in camaras)
+		{
+			cam.GetComponent<Camera>().enabled = !cam.GetComponent<Camera>().enabled;
+		}
+	}
+}
