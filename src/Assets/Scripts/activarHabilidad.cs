c6641ab6bb3d6d5c0eccc0b42164eee568272c73
Joseph Zamora <joseph.zamora64@yahoo.com>
2018-08-18 14:16:07 -0600

    Modelos de misil y de nave enemiga, habilidad de misil del jugador

---
+++ src/Assets/Scripts/activarHabilidad.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class activarHabilidad : MonoBehaviour {
+
+	#region Variables
+	[SerializeField]
+	private KeyCode tecla;
+	[SerializeField]
+	private GameObject misil;
+    #endregion
+
+    #region Metodos de Unity
+    void Start () {
+        
+    }
+    
+    void Update () {
+		if (Input.GetKeyDown(tecla))
+		{
+			Quaternion rotacionDeseada = Quaternion.Euler(new Vector3(0, 0, 0));
+			Instantiate(misil, transform.position, rotacionDeseada);
+		}
+    }
+    #endregion
+}
---
+++ src/Assets/movimientCuboTMP.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class movimientCuboTMP : MonoBehaviour {
+
+	#region Variables
+	[SerializeField]
+	private float speed;
+	private int side = 1;
+    #endregion
+
+    #region Metodos de Unity
+    void Start () {
+        
+    }
+    
+    void Update () {
+		if (gameObject.transform.position.x < -800)
+		{
+			side = 1;
+		}
+		else if (gameObject.transform.position.x > 800)
+		{
+			side = -1;
+		}
+		gameObject.transform.Translate(new Vector3(1,0) * speed * side);
+	}
+    #endregion
+}
